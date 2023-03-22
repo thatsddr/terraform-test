1ba55e25ef002ca4fdbad30e61be4b8c22fd279f
thatsddr <domenico.diruocco@code.berlin>
2023-03-22 17:04:09 +0100

    fix: set access variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,6 @@ variable "name" {
   type        = string
   description = "The resource name"
 }
+
+variable "access_key" { }
+variable "secret_key" { }
--- versions.tf
+++ versions.tf
@@ -9,4 +9,6 @@ terraform {
 
 provider "aws" {
   region = "eu-central-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
